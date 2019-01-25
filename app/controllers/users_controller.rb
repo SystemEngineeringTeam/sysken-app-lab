397d8b1d3ae863ab1e62aad9a7ad8a09c7b28651
KomuraHijiri <k16044kk@aitech.ac.jp>
2019-01-25 22:57:25 +0900

    【変更】コードをきれいにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
-       log_in @user
+      log_in @user
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-belongs_to :grade
+    belongs_to :grade
     belongs_to :gender
-    belongs_to :lab
-
-
+    belongs_to :lab 
 end
